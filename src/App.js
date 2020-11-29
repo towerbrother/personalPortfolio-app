217bc576e8cae76e7f775964cb505573e6e5480c
Giorgio Torre <giorgio.torre8@gmail.com>
2020-11-29 12:49:44 +0000

    Styled AboutSection using styled components. Created GlobalStyle.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import AboutPage from "./pages/AboutPage";
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutPage />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,34 +1,66 @@
 import React from "react";
 import home1 from "../img/home1.png";
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div className="about">
-      <div className="description">
+    <StyledAbout>
+      <StyledDescription>
         <div className="title">
-          <div className="hide">
+          <StyledHide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </StyledHide>
+          <StyledHide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </StyledHide>
+          <StyledHide>
             <h2>true.</h2>
-          </div>
+          </StyledHide>
         </div>
         <p>
           Contact us for any photography ideas you have. We have professionals
           with amazing skills.
         </p>
         <button>Contact Us</button>
-      </div>
-      <div className="image">
+      </StyledDescription>
+      <StyledImage>
         <img src={home1} alt="guy with a camera" />
-      </div>
-    </div>
+      </StyledImage>
+    </StyledAbout>
   );
 };
 
 export default AboutSection;
+
+const StyledAbout = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const StyledDescription = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const StyledImage = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const StyledHide = styled.div`
+  overflow: hidden;
+`;
