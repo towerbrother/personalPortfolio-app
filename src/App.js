84a55928dbc7d7e2ef03ba4ec9e0b3808583d992
Giorgio Torre <giorgio.torre8@gmail.com>
2020-11-29 10:04:08 +0000

    AboutSection and AboutPage initial setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio</h1>
+      <AboutPage />
     </div>
   );
 }
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutSection from "./../components/AboutSection";
+
+const AboutPage = () => {
+  return (
+    <>
+      <AboutSection />
+      {/* <ServicesSection /> */}
+      {/* <FAQSection/> */}
+    </>
+  );
+};
+
+export default AboutPage;
