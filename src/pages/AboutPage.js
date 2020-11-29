c6a4230115a4bc6ac00bc37a3732c7659b0dda38
Giorgio Torre <giorgio.torre8@gmail.com>
2020-11-29 10:15:50 +0000

    ServicesSection initial setup

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import AboutSection from "./../components/AboutSection";
+import ServicesSection from "./../components/ServicesSection";
 
 const AboutPage = () => {
   return (
     <>
       <AboutSection />
-      {/* <ServicesSection /> */}
+      <ServicesSection />
       {/* <FAQSection/> */}
     </>
   );
