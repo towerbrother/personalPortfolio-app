8f25170d05219d6131aa3b4ad3d77068d6f43fec
Giorgio Torre <giorgio.torre8@gmail.com>
2020-11-29 11:40:30 +0000

    FAQSection initial setup

---
+++ src/components/FAQSection.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+const FAQSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus quidem aut consequatur adipisci dolore non voluptatum
+            repellat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Recusandae, labore!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus quidem aut consequatur adipisci dolore non voluptatum
+            repellat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Recusandae, labore!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different Payment Method</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus quidem aut consequatur adipisci dolore non voluptatum
+            repellat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Recusandae, labore!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Products do you offer?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus quidem aut consequatur adipisci dolore non voluptatum
+            repellat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Recusandae, labore!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FAQSection;
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import AboutSection from "./../components/AboutSection";
 import ServicesSection from "./../components/ServicesSection";
+import FAQSection from "./../components/FAQSection";
 
 const AboutPage = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
-      {/* <FAQSection/> */}
+      <FAQSection />
     </>
   );
 };
